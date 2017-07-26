bf135b8981eb5cd5b5117cda742ec9198838c565
Daniel Pencak <danielpencak@gmail.com>
2017-07-26 14:58:15 -0500

    Cleanup card component

--- src/FrameworkCard/FrameworkCard.js
+++ src/FrameworkCard/FrameworkCard.js
@@ -5,83 +5,71 @@ import vueLogo from './Assets/vue.png';
 import angularLogo from './Assets/angular.png';
 import emberLogo from './Assets/ember.png';
 
-
-class FrameworkCard extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-    }
-  }
-  render() {
-    return (
-      <div className="FrameworkCard">
-        {
-          this.props.frameworkData ?
-          <div className="imageWrapper">
-            {
-              this.props.frameworkData.name === 'vue' ?
-              <div className="vue">
-                <img className="vueLogo" src={vueLogo} alt={"Vue"} />
-              </div>
-              :null
-            }
-            {
-              this.props.frameworkData.name === 'ember.js' ?
-              <div className="ember">
-                <img className="emberLogo" src={emberLogo} alt={"Ember"} />
-              </div>
-              :null
-            }
-            {
-              this.props.frameworkData.name === 'react' ?
-              <div className="react">
-                <img className="reactLogo" src={reactLogo} alt={"React"} />
-              </div>
-              :null
-            }
-            {
-              this.props.frameworkData.name === 'angular.js' ?
-              <div className="angular">
-                <img className="angularLogo" src={angularLogo} alt={"Angular"} />
-              </div>
-              :null
-            }
+export default function FrameworkCard(props) {
+  return (
+    <div className="FrameworkCard">
+      {
+        props.frameworkData ?
+        <div className="imageWrapper">
+          {
+            props.frameworkData.name === 'vue' ?
+            <div className="vue">
+              <img className="vueLogo" src={vueLogo} alt={"Vue"} />
+            </div>
+            :null
+          }
+          {
+            props.frameworkData.name === 'ember.js' ?
+            <div className="ember">
+              <img className="emberLogo" src={emberLogo} alt={"Ember"} />
+            </div>
+            :null
+          }
+          {
+            props.frameworkData.name === 'react' ?
+            <div className="react">
+              <img className="reactLogo" src={reactLogo} alt={"React"} />
+            </div>
+            :null
+          }
+          {
+            props.frameworkData.name === 'angular.js' ?
+            <div className="angular">
+              <img className="angularLogo" src={angularLogo} alt={"Angular"} />
+            </div>
+            :null
+          }
+        </div>
+        :null
+      }
+      {
+        props.frameworkData ?
+        <div className="cardInfo">
+          <div>
+            <p>
+              Forks
+            </p>
+            <p>
+              {props.frameworkData.forks_count}
+            </p>
           </div>
-          :null
-        }
-        {
-          this.props.frameworkData ?
-          <div className="cardInfo">
-            <div>
-              <p>
-                Forks
-              </p>
-              <p>
-                {this.props.frameworkData.forks_count}
-              </p>
-            </div>
-            <div>
-              <p>
-                Stargazers
-              </p>
-              <p>
-                {this.props.frameworkData.stargazers_count}
-              </p>
-            </div>
-            <div>
-              Open Issues
-              <p>
-                {this.props.frameworkData.open_issues_count}
-              </p>
-            </div>
+          <div>
+            <p>
+              Stargazers
+            </p>
+            <p>
+              {props.frameworkData.stargazers_count}
+            </p>
           </div>
-          :null
-        }
-      </div>
-    );
-  }
+          <div>
+            Open Issues
+            <p>
+              {props.frameworkData.open_issues_count}
+            </p>
+          </div>
+        </div>
+        :null
+      }
+    </div>
+  );
 }
-
-export default FrameworkCard;
